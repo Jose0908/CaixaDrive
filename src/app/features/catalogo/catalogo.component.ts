2e2ab5e10e58f518f4ae2933d8d99bd7e2253b0a
Jose Roberto <joserisoares2001@gmail.com>
2026-03-02 13:58:38 -0300

    feat: barra de pesquisa adicionada ao catalogo

--- src/app/features/catalogo/catalogo.component.ts
+++ src/app/features/catalogo/catalogo.component.ts
@@ -1,19 +1,28 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
 import { Veiculo } from '../../models/veiculos.model';
 import { VeiculoCardComponent } from '../veiculo-card/veiculo-card.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-catalogo',
   standalone: true,
-  imports: [CommonModule, VeiculoCardComponent],
+  imports: [CommonModule, VeiculoCardComponent, FormsModule],
   templateUrl: './catalogo.component.html',
   styleUrls: ['./catalogo.component.scss']
 })
 export class CatalogoComponent {
 
   private route = inject(ActivatedRoute);
-
   veiculos: Veiculo[] = this.route.snapshot.data['veiculos'];
+
+  termoPesquisa: string = '';
+
+  veiculosFiltrados() {
+    return this.veiculos.filter(v =>
+      v.modelo.toLowerCase().includes(this.termoPesquisa.toLowerCase())
+    );
+  }
+
 }
